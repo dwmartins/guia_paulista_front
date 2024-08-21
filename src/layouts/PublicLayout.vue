34e45a107d240d392070479ecfcbe02fd0e3a844
Douglas Martins <douglas5422@outlook.com>
2024-08-20 23:03:20 -0300

    Realizando um lop para valdiar status do site

--- src/layouts/PublicLayout.vue
+++ src/layouts/PublicLayout.vue
@@ -11,6 +11,33 @@
 <script setup>
 import AppHeader from '@/components/public/appHeader.vue';
 import appFooter from '@/components/public/appFooter.vue';
+import SiteInfoService from "@/services/SiteInfoService";
+import { settingsStore } from "@/store/SettingsStore";
+import { siteInfoStore } from "@/store/siteInfoStore";
+import { onMounted } from 'vue';
+import { router } from '@/router';
+import { showText } from '@/translation';
+
+onMounted( async () => {
+    await getSiteInfo();
+})
+
+const getSiteInfo = async () => {
+
+    setInterval(async () => {
+        const response = await SiteInfoService.getSiteInfo();
+        siteInfoStore.updateConstants(response.data.siteInfo);
+        settingsStore.setSettings(response.data.settings);
+        console.log(response.data.settings);
+        
+        if(settingsStore.getSetting('maintenance') === 'on') {
+            router.push(showText('PATH_MAINTENANCE'));
+        }
+
+    }, 120000); 
+
+};
+
 </script>
 
 <style>
